5cbb3b0b13a9f862200a1eca5ff1dee0bfe91618
NullXed <definenx0204@gmail.com>
2024-09-26 13:16:54 +0800

    Deployed  with MkDocs version: 1.6.0

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -8,3 +8,20 @@ const updateScheme = e => {
         "https://giscus.app"
     )
 }
+
+window.MathJax = {
+    tex: {
+      inlineMath: [["\\(", "\\)"]],
+      displayMath: [["\\[", "\\]"]],
+      processEscapes: true,
+      processEnvironments: true
+    },
+    options: {
+      ignoreHtmlClass: ".*|",
+      processHtmlClass: "arithmatex"
+    }
+  };
+  
+  document$.subscribe(() => { 
+    MathJax.typesetPromise()
+  })
